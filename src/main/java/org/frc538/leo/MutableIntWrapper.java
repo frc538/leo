ee4d377658cb2ca3835ec55388b07555d3ef4389
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2026-07-11 20:36:18 -0500

    Library test (#5)
    tag build branches

---
+++ src/main/java/org/frc538/leo/MutableIntWrapper.java
@@ -0,0 +1,26 @@
+package org.frc538.leo;
+
+import com.revrobotics.MutableInt;
+
+/** Test for loading REVLib */
+public class MutableIntWrapper {
+  private MutableInt _value;
+
+  /**
+   * Create a MutableInt
+   *
+   * @param value initial value
+   */
+  public MutableIntWrapper(int value) {
+    _value = new MutableInt(value);
+  }
+
+  /**
+   * Get the current value
+   *
+   * @return the current value
+   */
+  public int get() {
+    return _value.value;
+  }
+}
---
+++ src/test/java/org/frc538/leo/MutableIntWrapperTest.java
@@ -0,0 +1,18 @@
+package org.frc538.leo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MutableIntWrapperTest {
+  @Test
+  void createValue() {
+    new MutableIntWrapper(1);
+  }
+
+  @Test
+  void getValue() {
+    var it = new MutableIntWrapper(1);
+    assertEquals(1, it.get());
+  }
+}
